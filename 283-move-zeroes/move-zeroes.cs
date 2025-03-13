d6c89585817940ee2d053b38ebc2be94a7225e8f
Emerson <emerson56@users.noreply.github.com>
2025-03-12 23:47:59 -0400

    Time: 2 ms (30.81%) | Memory: 58.4 MB (34.76%) - LeetSync

--- 283-move-zeroes/move-zeroes.cs
+++ 283-move-zeroes/move-zeroes.cs
@@ -1,21 +1,24 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
-        // initialize 2 pointers 
-        int fast = 0;
-        int nextP = 0; // points to the next location of the last seen non-zero
-
+        // set 2 pointers
+        int fast = 0, nextPtr = 0;
+        // iterate through the array
+        // goal move all the non-zeroes to the left
         while (fast < nums.Length)
         {
+            /*
+                non-zero goes to the left
+                0 1 3
+            */
             if(nums[fast]!=0)
             {
-                nums[nextP] = nums[fast];
-                nextP++;
+                nums[nextPtr++] = nums[fast];
             }
             fast++;
         }
 
-        // Fill in the rest
-        for(int i = nextP; i < nums.Length; i++)
+        // fill the rest of the elements with 0
+        for(int i = nextPtr; i < nums.Length; i++)
         {
             nums[i] = 0; 
         }
