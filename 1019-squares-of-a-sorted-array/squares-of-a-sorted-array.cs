4ddef9344fd62120ba53abebc2d2a7afc6f022c8
Emerson <emerson56@users.noreply.github.com>
2025-03-12 23:55:57 -0400

    Time: 1 ms (100.00%) | Memory: 64.2 MB (97.65%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
@@ -1,27 +1,24 @@
 public class Solution {
     public int[] SortedSquares(int[] nums) {
-        // start from the end to ensure the resulting array is also sorted
-        int i = nums.Length - 1; 
-        int left = 0; 
-        int right = i; 
+        // key insights: 
+        // the largest elements are at the edges
+        // 
+
+        int left = 0, right = nums.Length - 1; 
         var res = new int[nums.Length];
 
-        /*
-        Key insight: numbers at the of array will produce bigger number because input array is 
-        sorted and we only focu on numbers. 
-        */
-        while (i >= 0)
+        for(int i = res.Length -1; i >=0; i--)
         {
-            if(Math.Abs(nums[left]) > Math.Abs(nums[right])){
+            if(Math.Abs(nums[right]) > Math.Abs(nums[left]))
+            {
+                res[i] = nums[right] * nums[right];
+                right--;
+            }
+            else
+            {
                 res[i] = nums[left] * nums[left];
                 left++;
             }
-            else{
-               res[i] = nums[right] * nums[right];
-               right--; 
-            }
-
-            i--;
         }
 
         return res; 
